5591cdbbde8f468b617a70000f1d5d6bf5925b92
Dipanshu Rawat <dipanshurawat4@gmail.com>
2026-06-14 18:05:34 +0530

    imported data from supabase and set Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FactList from "./components/FactList";
 import Header from "./components/Header";
 import NewFactForm from "./components/NewFactForm";
 import supabase from "./supabase";
+import Loader from "./components/Loader";
 
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
@@ -17,48 +18,21 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 export default function App() {
   const [formClass, setFormClass] = useState("fact-form hidden");
-  const [facts, setFacts] = useState(initialFacts);
+  const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getFacts() {
+      setIsLoading(true);
       const { data: facts, error } = await supabase.from("facts").select("*");
-      console.log(facts);
+      if (error) {
+        alert(error);
+      }
+
+      setFacts(facts);
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -75,11 +49,11 @@ export default function App() {
       />
       <main className="main">
         <CategoryFilters CATEGORIES={CATEGORIES} />
-        <FactList
-          initialFacts={initialFacts}
-          CATEGORIES={CATEGORIES}
-          facts={facts}
-        />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <FactList CATEGORIES={CATEGORIES} facts={facts} />
+        )}
       </main>
     </>
   );
